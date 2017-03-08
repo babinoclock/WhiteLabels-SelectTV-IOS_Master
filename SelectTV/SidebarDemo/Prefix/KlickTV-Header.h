7411d88669ba20a8b363774f672b841f6ae27780
ocs.mac <ocs.mac@ocsmacs-mac-mini.local>
2017-03-08 18:07:06 +0530

    added klickTV target

---
+++ SelectTV/SidebarDemo/Prefix/KlickTV-Header.h
@@ -0,0 +1,15 @@
+//
+//  KlickTV-Header.h
+//  SidebarDemo
+//
+//  Created by ocs.mac on 3/8/17.
+//  Copyright © 2017 Appcoda. All rights reserved.
+//
+
+#define APP_TITLE                           @"KlickTV"
+
+#define SplashScreenImageName               @"KlickTV_SplashScreenImage"
+
+#define homeLogoImageName                   @"KlickTV_homeLogo"
+
+#define splashLogoImageName                 @"KlickTV_splash_logo"
