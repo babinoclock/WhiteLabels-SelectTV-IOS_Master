495958dd3487978086eef95042d3655a532529c4
Mohan Raj <ocs.mac@ocsmacs-mac-mini.local>
2017-03-21 17:01:32 +0530

    Added Target HughesNet.

---
+++ SelectTV/SidebarDemo/Prefix/HughesNet-Header.h
@@ -0,0 +1,21 @@
+//
+//  HughesNet-Header.h
+//  SidebarDemo
+//
+//  Created by ocs.mac on 3/21/17.
+//  Copyright © 2017 Appcoda. All rights reserved.
+//
+
+#define APP_TITLE                           @"HughesNet"
+
+#define SplashScreenImageName               @"HughesNet_SplashScreenImage"
+
+#define homeLogoImageName                   @"HughesNet_homeLogo"
+
+#define splashLogoImageName                 @"HughesNet_splash_logo"
+
+//#define splashLogoImageName1                 @"KlickTV_CustomLaunchIcon"
+
+
+#define splashNewImage                      @"HughesNet_CustomLaunchIcon"
+
